466edb93789e8e6c93fff8b22de006c96e887cd8
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-04-16 15:36:28 -0300

    Botones

--- src/components/HeaderHome.js
+++ src/components/HeaderHome.js
@@ -30,21 +30,50 @@ const HeaderHome = () => {
 
     return (
         <div className="bannerHome">
-            <div className='flex justify-between items-center'>
-                <nav className='flex justify-between gap-2 mt-12 ml-auto mr-10'>
-                    {links.map(link => {
-                        return (
-                            <Link
-                                key={link.label}
-                                href={link.href}
-                                className={`${pathname === link.href ?
-                                    'font-bold underline underline-offset-8 decoration-red-600' : ''} 
+
+            <div class="grid grid-cols-6 mr-4 pt-12 text-center justify-items-center">
+
+                {/* Registro e inicio de sesion */}
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+
+                <div >
+                    <Image
+                        src={"/images/botones/inicio_sesion.png"}
+                        alt="Iniciar sesion"
+                        width={160}
+                        height={50}
+                    />
+                </div>
+
+                <div>
+                    <Image
+                        src={"/images/botones/inicio_sesion.png"}
+                        alt="Registrarse"
+                        width={160}
+                        height={50}
+                    />
+                </div>
+
+
+                {/* Menu de navegacion */}
+                <div></div>
+                <div></div>
+                {links.map(link => {
+                    return (
+                        <Link
+                            key={link.label}
+                            href={link.href}
+                            className={`${pathname === link.href ?
+                                'font-bold underline underline-offset-8 decoration-red-600' : ''} 
                                         text-base p-3 text-white menuInicio`}>
-                                {link.label}
-                            </Link>
-                        )
-                    })}
-                </nav>
+                            {link.label}
+                        </Link>
+                    )
+                })}
+
             </div>
 
             <Link href={"https://api.whatsapp.com/send?phone=59899387921&text=Hola! ¿Qué%20tal? Te escribo desde la aplicación web de Brinna!"}>
@@ -52,8 +81,8 @@ const HeaderHome = () => {
                     <Image
                         src={"/images/icono_whatsapp.png"}
                         alt="Contacto whatsapp"
-                        width={100}
-                        height={50}
+                        width={88}
+                        height={88}
                     />
                 </div>
             </Link>
