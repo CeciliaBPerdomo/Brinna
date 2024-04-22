3628ff1fd8607706da4fb2440ce29d3c6a9c36e0
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-04-22 14:48:32 -0300

    Diseno del newsletter

--- src/components/LandingPage/NewsLetter/news.js
+++ src/components/LandingPage/NewsLetter/news.js
@@ -1,8 +1,14 @@
 import "../NewsLetter/newsLetter.css"
 import { Jost } from "next/font/google"
+import Image from "next/image"
 
 const jost = Jost({
     weight: "700",
+    subsets: ['latin'],
+})
+
+const sub_jost = Jost({
+    weight: "500",
     subsets: ['latin'],
 })
 
@@ -12,6 +18,34 @@ const News = () => {
             <p className={`mantente ${jost.className}`}>
                 ¡Mantente al tanto!
             </p>
+
+            <div className="flex mb-4 justify-center">
+                <div className="w-1/2 suscribite">
+                    <p className={`letraSuscribite ${sub_jost.className}`}>
+                        Suscríbite a nuestro newsletter
+                        y entérate de las últimas novedades.
+                    </p>
+                </div>
+
+                <div className="my-auto">
+                    <div className="w-1/2 inputDiv">
+                        <input
+                            type="text"
+                            className="rounded inputNews"
+                            placeholder="Ingresa tu correo electrónico"
+                        />
+                    </div>
+                    <div className="w-1/2 divBoton">
+                        <Image
+                            src={"/images/botones/suscribirme.png"}
+                            alt={"Suscribirme"}
+                            width={122}
+                            height={42}
+                        />
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
