be34dd7907f22be503907f5613c1e36aa18c7ae3
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-04-26 12:11:40 -0300

    Responsive celulares margen comprar/vender

--- src/components/LandingPage/ComprarVender/ComprarVender.js
+++ src/components/LandingPage/ComprarVender/ComprarVender.js
@@ -11,25 +11,21 @@ const ComprarVender = () => {
 
     return (
         <div className="principal_cv">
-            {/* <div className="grid grid-cols-2 grid_cv"> */}
-
-                <div className="bg-comprar fondo_cv">
-                    <div className="boton_cv">
-                        <button className="titulo_cv jost">
-                            COMPRAR
-                        </button>
-                    </div>
+            <div className="bg-comprar fondo_cv">
+                <div className="boton_cv">
+                    <button className="titulo_cv jost">
+                        COMPRAR
+                    </button>
                 </div>
+            </div>
 
-                <div className="bg-vender fondo_cv">
-                    <div className="boton_cv">
-                        <button className="titulo_cv jost">
-                            VENDER
-                        </button>
-                    </div>
+            <div className="bg-vender fondo_cv">
+                <div className="boton_cv">
+                    <button className="titulo_cv jost">
+                        VENDER
+                    </button>
                 </div>
-
-            {/* </div> */}
+            </div>
         </div>
     )
 }
