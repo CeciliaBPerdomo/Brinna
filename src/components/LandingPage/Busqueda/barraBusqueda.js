0e380d7ef8aec11888a72461b0378f0894527037
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-05-21 13:29:10 -0300

    Responsive barra

--- src/components/LandingPage/Busqueda/barraBusqueda.js
+++ src/components/LandingPage/Busqueda/barraBusqueda.js
@@ -15,7 +15,7 @@ const jost_barra = Jost({
 const Barra = () => {
 
     return (
-        <div>
+        <div className="barra_busqueda_contenedor">
 
             {/* Encabezado */}
             <div className="texto_barra">
@@ -31,8 +31,15 @@ const Barra = () => {
                         className={`input_barra ${jost_barra}`}
                     />
                     <button className="boton_lupa_barra">
-                        <svg className="h-8 w-8 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                        <svg className="text-white svg_boton_lupa_barra"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                         </svg>
                     </button>
                 </div>
