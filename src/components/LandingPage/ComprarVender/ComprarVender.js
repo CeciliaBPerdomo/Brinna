b75394fcbf5add2d4ddd21cc897abb599e0af761
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-05-21 15:43:48 -0300

    Responsive comprar vender v.I

--- src/components/LandingPage/ComprarVender/ComprarVender.js
+++ src/components/LandingPage/ComprarVender/ComprarVender.js
@@ -12,21 +12,24 @@ const ComprarVender = () => {
 
     return (
         <div className="principal_cv">
-            <div className="bg-comprar fondo_cv">
-                <div className="boton_cv">
-                    <button className={`titulo_cv ${jost}`}>
-                        COMPRAR
-                    </button>
+            <div class="grid grid-cols-2 gap-2">
+                <div className="bg-comprar fondo_cv">
+                    <div className="boton_cv">
+                        <button className={`titulo_cv ${jost}`}>
+                            COMPRAR
+                        </button>
+                    </div>
+                </div>
+
+                <div className="bg-vender fondo_cv">
+                    <div className="boton_cv">
+                        <button className={`titulo_cv ${jost}`}>
+                            VENDER
+                        </button>
+                    </div>
                 </div>
             </div>
 
-            <div className="bg-vender fondo_cv">
-                <div className="boton_cv">
-                    <button className={`titulo_cv ${jost}`}>
-                        VENDER
-                    </button>
-                </div>
-            </div>
         </div>
     )
 }
