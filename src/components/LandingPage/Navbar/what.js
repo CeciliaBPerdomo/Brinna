576a6c63dd649a35ab446f8ecd3e6c471602789e
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-05-30 15:06:40 -0300

    Logo whatsapp - con responsive obviamente

--- src/components/LandingPage/Navbar/what.js
+++ src/components/LandingPage/Navbar/what.js
@@ -8,13 +8,13 @@ const LogoWhatsapp = () => {
     return (
         <div>
             <Link href={"https://api.whatsapp.com/send?phone=59899387921&text=Hola! ¿Qué%20tal? Te escribo desde la aplicación web de Brinna!"}>
-                <div>
+                <div className='logoWhatsapp'>
                     <Image
                         src={"/images/icono_whatsapp.png"}
                         alt="Contacto whatsapp"
                         width={88}
                         height={88}
-                        className='logoWhatsapp'
+                        
                     />
                 </div>
             </Link>
