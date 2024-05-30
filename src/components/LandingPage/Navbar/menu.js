3fe5114ec820ed72f217d34624221e6833c5ccb6
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-05-30 14:35:36 -0300

    Probando el menu

--- src/components/LandingPage/Navbar/menu.js
+++ src/components/LandingPage/Navbar/menu.js
@@ -28,8 +28,8 @@ const Menu = () => {
     ]
 
     return (
-        <div className='menuHeader'>
-        <nav className="menuNav">
+        <div className='menuHeader_menuHomePrincipal'>
+        <nav className="menuNav_menuHomePrincipal">
             {links.map(link => {
                 return (
                     <Link
@@ -37,7 +37,7 @@ const Menu = () => {
                         href={link.href}
                         className={`${pathname === link.href ?
                             'font-bold underline underline-offset-8 decoration-red-600' : ''} 
-                                        text-base p-3 menuInicio`}>
+                                        text-base p-3 menuInicio_menuHomePrincipal`}>
                         {link.label}
                     </Link>
                 )
