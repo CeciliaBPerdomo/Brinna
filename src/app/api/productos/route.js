a1f2382318af647c566620c22eb7e42c370e73eb
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-06-21 14:53:48 -0300

    Prueba categoria para ellas

--- src/app/api/productos/route.js
+++ src/app/api/productos/route.js
@@ -1,21 +1,21 @@
-import { NextResponse } from "next/server";
-import { collection, getDocs, query, where } from "firebase/firestore";
-import { db } from "@/app/firebase/config";
+// import { NextResponse } from "next/server";
+// import { collection, getDocs, query, where } from "firebase/firestore";
+// import { db } from "@/app/firebase/config";
 
 
-export async function GET(request, {params}) {
-    const {categoria} = params
+// export async function GET(request, {params}) {
+//     const {categoria} = params
     
-    const productosRef = collection(db, "productos")
+//     const productosRef = collection(db, "productos")
     
-    const q = categoria === "todos"
-    ?
-    productosRef
-    : 
-    query(productosRef, where("type", "==", categoria))
+//     const q = categoria === "todos"
+//     ?
+//     productosRef
+//     : 
+//     query(productosRef, where("categoria", "==", categoria))
 
-    const querySnapShot = await getDocs(q)
-    const docs = querySnapShot.docs.map(doc => doc.data())
+//     const querySnapShot = await getDocs(q)
+//     const docs = querySnapShot.docs.map(doc => doc.data())
 
-    return NextResponse.json(docs)
-}
+//     return NextResponse.json(docs)
+// }
