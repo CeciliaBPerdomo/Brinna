4def2e4f4056abc413017477e8cddf8f588c0f0d
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-08-23 15:15:13 -0300

    Logueo

---
+++ src/components/Auth/InicioSesion/FormInicioSesion.js
@@ -0,0 +1,217 @@
+"use client"
+import React, { useState } from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation';
+
+//Redux
+import { useDispatch } from 'react-redux';
+import { loginUsuario } from '../../../lib/usuariosSlice';
+
+// Alerts
+import { ToastContainer, toast, Bounce } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+// CSS
+import "../InicioSesion/formInicioSesion.css"
+
+// Fuente
+import { Jost } from "next/font/google"
+
+const jost = Jost({
+    weight: "600",
+    subsets: ['latin'],
+})
+
+const ya_jost = Jost({
+    weight: "500",
+    subsets: ['latin'],
+})
+
+const olvidaste_jost = Jost({
+    weight: "100",
+    subsets: ['latin'],
+})
+
+
+function FormInicioSesion() {
+    const router = useRouter();
+    const dispatch = useDispatch();
+
+    const [loading, setLoading] = useState(false);
+
+    // Para guardar la info
+    const initialValues = {
+        email: "",
+        password: ""
+    }
+
+    const [values, setValues] = useState(initialValues)
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setValues({
+            ...values,
+            [name]: value
+        });
+    }
+
+    // Chequea que se ingresen los datos
+    const handleChequeo = () => {
+        // Verificar si los campos están completos
+        if (!values.email) {
+            setLoading(false);
+            tostada("Por favor ingresa tu correo electrónico.")
+            return false
+        }
+
+        if (!values.password) {
+            setLoading(false);
+            tostada("Por favor ingresa tu contraseña.")
+            return false
+        }
+        return true
+    }
+
+    function tostada(mensaje) {
+        toast.error(mensaje, {
+            position: "top-center",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+            transition: Bounce,
+        });
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        setLoading(true);  // Activar el loader
+        let resp = handleChequeo()
+
+        try {
+            if (resp) {
+                // Disparar la acción de inicio de sesión
+                await dispatch(loginUsuario(values)).unwrap();
+                console.info("Usuario logueado exitosamente");
+
+                setValues(initialValues)
+                setLoading(false);
+                router.push('/')
+            }
+        } catch (error) {
+            setLoading(false);
+            tostada(error)
+        }
+    }
+
+    return (
+        <>
+            <div className="fixed inset-0 flex justify-center items-center">
+                <div className='div_formulario_iniciosesion_principal flex'>
+                    {/* Imagen logueo */}
+                    <div className="w-1/2 div_formulario_iniciosesion_imagen">
+                        <Image
+                            src={"/images/registro.png"}
+                            fill
+                            alt='Registro'
+                            className='formulario_iniciosesion_imagen'
+                            style={{ objectFit: 'cover' }}
+                            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                            priority
+                        />
+                    </div>
+
+
+                    <div className="w-1/2 p-6 div_iniciosesion_registro">
+                        <h2 className={`iniciar_sesion ${jost}`}>Iniciar sesión</h2>
+                        <p className={`completa_tus_datos_sesion ${jost}`}>Completa tus datos para ingresar a tu cuenta.</p>
+
+                        <form
+                            className='formulario_iniciarsesion_inputs'
+                            onSubmit={handleSubmit}
+                        >
+                            {/* Email */}
+                            <div className="flex items-center">
+                                <input
+                                    className="shadow appearance-none border focus:outline-none focus:shadow-outline 
+                                    iniciosesion_inputs"
+                                    type="email"
+                                    placeholder="Correo electrónico"
+                                    name="email"
+                                    value={values.email}
+                                    onChange={handleChange}
+                                />
+                            </div>
+
+                            {/* Contraseña */}
+                            <div className="flex items-center">
+                                <input
+                                    className="shadow appearance-none border focus:outline-none focus:shadow-outline 
+                                    iniciosesion_inputs"
+                                    type="password"
+                                    placeholder="Contraseña"
+                                    name="password"
+                                    value={values.password}
+                                    onChange={handleChange}
+                                />
+                            </div>
+
+                            <p className={`olvidaste_contra ${olvidaste_jost}`}>¿Olvidaste tu contraseña?</p>
+
+                            {/* Boton de iniciar sesion */}
+                            <div className='flex justify-center'>
+                                <div className="flex items-center justify-center div_botonGuardar_ingresar">
+                                    <button
+                                        className="text-white font-bold py-2 px-4 rounded focus:outline-none 
+                                    focus:shadow-outline botonGuardar_ingresar"
+                                        type="button"
+                                        onClick={(e) => handleSubmit(e)}
+                                    >
+                                        Ingresar
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+
+                        <div className='flex items-center justify-center'>
+                            <p className={`no_tengo_cuenta ${ya_jost}`}>Aún no tengo cuenta.</p>
+                        </div>
+
+                        <div className='flex items-center justify-center'>
+                            <p className={`ingresar_o ${jost}`}> ______________ o ______________ </p>
+                        </div>
+
+                        <div className='flex items-center justify-center div_iniciar_con_google'>
+                            <button className='boton_iniciarsesion_con_google'>
+                                <Image
+                                    src={"/images/icono_google.png"}
+                                    width={29}
+                                    height={33}
+                                    alt="Google"
+                                    className='icono_google_iniciarsesion'
+                                />
+                                <span className='texto_iniciarsesion_google'>
+                                    Iniciar sesión con Google
+                                </span>
+                            </button>
+                        </div>
+
+                    </div>
+                </div>
+
+                {loading && (
+                    <div className="loader-overlay">
+                        <div className="loader"></div>
+                    </div>
+                )}
+
+                <ToastContainer />
+            </div>
+        </>
+    )
+}
+
+export default FormInicioSesion
--- src/components/LandingPage/Navbar/botones.js
+++ src/components/LandingPage/Navbar/botones.js
@@ -1,13 +1,18 @@
 "use client"
 {/* Registro e inicio de sesion */ }
 import Link from "next/link"
+
+//Redux
 import { useSelector } from "react-redux";
 
 // CSS
 import "../Navbar/botones.css"
-import { Jost } from "next/font/google"
+
+// Componente
 import MenuOpciones from "@/components/Catalogos/MenuOpciones";
 
+//Fuente
+import { Jost } from "next/font/google"
 const jost = Jost({
     weight: "400",
     subsets: ['latin'],
@@ -28,26 +33,31 @@ const BotonesSesion = () => {
                 ) : (
                     // Mostrar botones de registro e inicio de sesión si no está autenticado
                     <>
+                        {/* Registro */}
                         <Link href={"/admin/Registro"}>
                             <button className={`botones_registro ${jost}`}>Registrarte</button>
                         </Link>
-                        <button className={`botones_iniciar_sesion ${jost}`}>
-                            <svg xmlns="http://www.w3.org/2000/svg"
-                                width="30"
-                                height="30"
-                                viewBox="0 0 30 30"
-                                fill="none"
-                                className="icono_botones_iniciar_sesion"
-                            >
-                                <path d="M15 29C22.732 29 29 22.732 29 15C29 7.26801 22.732 1 15 1C7.26801 1 1 7.26801 1 15C1 22.732 7.26801 29 15 29Z"
-                                    stroke="white" strokeWidth="1.5" />
-                                <path d="M15 15C17.3197 15 19.2 13.1196 19.2 10.8C19.2 8.48039 17.3197 6.59998 15 6.59998C12.6804 6.59998 10.8 8.48039 10.8 10.8C10.8 13.1196 12.6804 15 15 15Z"
-                                    stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-                                <path d="M8 23.4V22C8 18.134 11.134 15 15 15C18.866 15 22 18.134 22 22V23.4"
-                                    stroke="white" strokeWidth="1.5" strokeLinecap="round" />
-                            </svg>
-                            Iniciar sesión
-                        </button>
+
+                        {/* Inicio de sesion */}
+                        <Link href={"/admin/IniciarSesion"}>
+                            <button className={`botones_iniciar_sesion ${jost}`}>
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                    width="30"
+                                    height="30"
+                                    viewBox="0 0 30 30"
+                                    fill="none"
+                                    className="icono_botones_iniciar_sesion"
+                                >
+                                    <path d="M15 29C22.732 29 29 22.732 29 15C29 7.26801 22.732 1 15 1C7.26801 1 1 7.26801 1 15C1 22.732 7.26801 29 15 29Z"
+                                        stroke="white" strokeWidth="1.5" />
+                                    <path d="M15 15C17.3197 15 19.2 13.1196 19.2 10.8C19.2 8.48039 17.3197 6.59998 15 6.59998C12.6804 6.59998 10.8 8.48039 10.8 10.8C10.8 13.1196 12.6804 15 15 15Z"
+                                        stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                    <path d="M8 23.4V22C8 18.134 11.134 15 15 15C18.866 15 22 18.134 22 22V23.4"
+                                        stroke="white" strokeWidth="1.5" strokeLinecap="round" />
+                                </svg>
+                                Iniciar sesión
+                            </button>
+                        </Link>
                     </>
                 )}
             </nav>
