71a573d1bc5ba3c2fb1b6c9c2fe6bf585b0a5a9c
Cecilia Perdomo <cecilia.perdomo@gmail.com>
2024-10-18 21:21:17 -0300

    Actualizar datos de perfil

--- src/components/MenuUsuario/construccion.js
+++ src/components/MenuUsuario/construccion.js
@@ -1,17 +1,22 @@
 import Image from "next/image"
 
+//Fuente
+import { Jost } from "next/font/google"
+const jost = Jost({
+  weight: "600",
+  subsets: ['latin']
+})
+
 function Construccion() {
   return (
     <div>
-         <div> 
-                <p>Esta sección se encuentra en construcción, desarrollo y planificación.</p>
-                <Image 
-                src={"/images/buscando.png"}
-                width={350}
-                height={50}
-                alt="Working"
-                />
-            </div>
+        <p className={`${jost} mt-3`}>Esta sección se encuentra en construcción, desarrollo y planificación.</p>
+        <Image
+          src={"/images/buscando.png"}
+          width={350}
+          height={50}
+          alt="Working"
+        />
     </div>
   )
 }
